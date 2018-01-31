30678412f1ad9782116ed24bec903c3ec74e2916
Benjamin Ditel <b.ditel@ideenwerft.com>
2018-01-31 10:16:04 +0100

    [TASK] Deactivated JS and PHP checks

--- remove-debugs.sh
+++ remove-debugs.sh
@@ -15,16 +15,16 @@
 . "./debugs.cfg"
 
 # Delete Javascript debugs
-for js in ${JS_DEBUGS[@]};
-do
-  #find ./ -type f -path ./_save -prune -o -name '*.js' -exec sed -i "/$js/d" {} \;
-done
+# for js in ${JS_DEBUGS[@]};
+# do
+#   find ./ -type f -path ./_save -prune -o -name '*.js' -exec sed -i "/$js/d" {} \;
+# done
 
 # Delete PHP debugs
-for php in ${PHP_DEBUGS[@]};
-do
-  #find ./ -path ./_save -prune -o -name '*.php*' -exec sed -i "/$php/d" {} \;
-done
+# for php in ${PHP_DEBUGS[@]};
+# do
+#   #find ./ -path ./_save -prune -o -name '*.php*' -exec sed -i "/$php/d" {} \;
+# done
 
 # Delete Fluid debugs
 for f in ${FLUID_DEBUGS[@]};
